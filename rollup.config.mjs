a1f4c799a2ad4c76f0dbbaa457daa6d1762a77ce
함승주 <hsj1514@kaist.ac.kr>
2024-04-04 13:04:39 +0900

    cjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -19,7 +19,7 @@ export default [
         input: './src/index.ts',
         output: {
             file: './dist/index.js',
-            format: 'iife',
+            format: 'cjs',
             sourcemap: true,
         },
         plugins: [
