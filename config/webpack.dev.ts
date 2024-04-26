b52215c525e2adf972f908f8a4ac6304aed6c04e
함승주 <hsj1514@kaist.ac.kr>
2024-04-26 19:06:10 +0900

    webpack dev

--- config/webpack.dev.ts
+++ config/webpack.dev.ts
@@ -1,32 +1,31 @@
 import path from "path";
 import webpack from "webpack";
-import "webpack-dev-server";
 import { merge } from "webpack-merge";
 import common from "./webpack.common";
+import WebpackDevServer from 'webpack-dev-server';
+
+// @ts-ignore
+declare module 'webpack' {
+    interface Configuration {
+        devServer?: WebpackDevServer.Configuration;
+    }
+}
+
+const fileName = 'dev.js';
 
 const configuration: webpack.Configuration = {
     mode: "development",
     devtool: "inline-source-map",
     output: {
         path: path.resolve(__dirname, "../dist"),
-        filename: "[name].bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-                exclude: /node_modules/,
-            },
-        ],
+        filename: fileName,
+        clean: true,
     },
     devServer: {
-        static: path.join(__dirname, "public"),
-        port: 3000,
+        static: path.join(__dirname, '../dist'),
+        port: 9000,
         open: true,
-        compress: true,
-        historyApiFallback: true,
-        hot: true
+        hot: true,
     },
     watchOptions: {
         ignored: /node_modules/,
