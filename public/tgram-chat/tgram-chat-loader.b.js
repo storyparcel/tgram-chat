e3f95a04b41944e59f0e13ead52bbe602800999b
함승주 <hsj1514@kaist.ac.kr>
2024-06-15 19:37:14 +0900

    tgram prod

--- public/tgram-chat/tgram-chat-loader.b.js
+++ public/tgram-chat/tgram-chat-loader.b.js
@@ -21,7 +21,7 @@
         }
 
         try {
-            const response = await fetch('https://cdn.jsdelivr.net/gh/storyparcel/tgram-chat/public/tgram-chat/tgram-chat.a.html');
+            const response = await fetch('https://cdn.jsdelivr.net/gh/storyparcel/tgram-chat/public/tgram-chat/tgram-chat.b.html');
             if (!response.ok) {
                 throw new Error('Failed to fetch HTML content');
             }
